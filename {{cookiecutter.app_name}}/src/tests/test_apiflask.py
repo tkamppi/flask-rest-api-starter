c1abeff72796726f72ae2e41b97484670c6f41d0
tkamppi <tkamppi@users.noreply.github.com>
2019-08-19 18:16:17 +0200

    Added more unit tests for apiflask.py

--- {{cookiecutter.app_name}}/src/tests/test_apiflask.py
+++ {{cookiecutter.app_name}}/src/tests/test_apiflask.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 
 from flask import Response
 
@@ -7,6 +8,25 @@ from {{cookiecutter.app_name}}.apiflask import ApiFlask, ApiResult, ApiException
 class TestApiFlask(unittest.TestCase):
 
     def test_apiflask_response_with_apiresult(self):
-        response = ApiFlask(__name__).make_response(ApiResult({'key1': 'value1'}))
-        #Remove. Add test for key1 / value1 in Response object (json) and for mimetype
+        response = ApiFlask(__name__).make_response(ApiResult({"key1": "value1"}))
+        json_response = json.loads(response.get_data())
         self.assertIsInstance(response, Response)
+        self.assertEqual(response.mimetype, "application/json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json_response["key1"], "value1")
+
+    def test_apiexception(self):
+        api_exception = ApiException("message")
+        api_result = api_exception.to_result()
+        response = api_result.to_response()
+        json_response = json.loads(response.get_data())
+        self.assertIsInstance(response, Response)
+        self.assertEqual(response.mimetype, "application/json")
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(json_response["error"], "message")
+
+    def test_apiflask_make_response(self):
+        app = ApiFlask(__name__)
+        with app.test_request_context():
+            response = app.make_response("any response")
+            self.assertTrue(response.mimetype, "text/html") #defaults to text/html
